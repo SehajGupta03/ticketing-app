c0fdb82b4b5befa14124686b2a9d62af3afa338f
Sehaj Gupta <sehajguptaxd@gmail.com>
2025-01-04 16:31:59 -0500

    Added Form
    Added form and backend function to post data. Connected the app to db.

---
+++ app/[components]/TicketForm.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = () => {
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setForData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    };
+    
+    const handleSubmit = () => {
+        console.log("Submitted");
+    };
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "Not Started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setForData] = useState(startingTicketData);
+
+  return (
+    <div className="flex justify-center">
+      <form
+        action=""
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create Your Ticket</h3>
+        <label htmlFor="">Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
